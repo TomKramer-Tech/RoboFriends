5b7758e55e12e743bf998247fed7605f4897d333
Tom Kramer <109452847+T-Kramer@users.noreply.github.com>
2022-10-26 14:13:25 -0500

    added styles. background animation, etc

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Card.css'
 
 const Card = ({ name, email, id }) => {
     return(
-        <div className='tc bg-light-blue dib br3 pa 3 ma2 grow bw2 shadow-5'>
+        <div className='card tc dib br3 pa 3 ma2 grow bw2 shadow-5'>
             <img alt='robot' src={`https://robohash.org/bartholomew${id}?200x200`} />
             <div>
                 <h2>{name}</h2>
